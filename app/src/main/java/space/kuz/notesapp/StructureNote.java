7cb6a5ccf83d2891c3231c6209d27f5ce70ac6ae
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-09-27 11:33:48 +0300

    Создания класса данных со структурой заметки: название заметки, описание заметки, дата создания

---
+++ app/src/main/java/space/kuz/notesapp/StructureNote.java
@@ -0,0 +1,53 @@
+package space.kuz.notesapp;
+
+import androidx.annotation.Nullable;
+
+import java.util.Date;
+
+public class StructureNote {
+    @Nullable
+    private Integer id;
+    private String head;
+    private String description;
+    private Date date;
+
+    public StructureNote(@Nullable Integer id, String head, String description, Date date) {
+        this.id = id;
+        this.head = head;
+        this.description = description;
+        this.date = date;
+    }
+
+    @Nullable
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(@Nullable Integer id) {
+        this.id = id;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
