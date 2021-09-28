e9ccf5a9cb5e6c39c8deaf2a25048b51c050affb
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-09-28 10:43:01 +0300

    Создание елементов разметки в EditNote

--- app/src/main/java/space/kuz/notesapp/ui/NoteEditActivity.java
+++ app/src/main/java/space/kuz/notesapp/ui/NoteEditActivity.java
@@ -1,16 +1,51 @@
 package space.kuz.notesapp.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 import space.kuz.notesapp.R;
 
 public class NoteEditActivity extends AppCompatActivity {
-
+    MaterialToolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_edit);
+        initToolBar();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note_edit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note_item: {
+                Intent intent = new Intent(this, NoteListActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            default: {
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+
+    private void initToolBar() {
+        toolbar = (MaterialToolbar) findViewById(R.id.note_list_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
